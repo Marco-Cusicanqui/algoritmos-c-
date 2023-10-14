98bd87de5a7afdb836cef699f4a8ae601ffa0b8b
Marco-Cusicanqui <52262380+Marco-Cusicanqui@users.noreply.github.com>
2023-10-14 14:33:14 -0400

    Update rerootingTemplate.cpp

--- Trees/rerootingTemplate.cpp
+++ Trees/rerootingTemplate.cpp
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void dfsPre(int u, int pa, ...){
+void dfsPre(int u, int pa){
   
 }
 
-void dfsReroot(int u, int pa, ...){
+void dfsReroot(int u, int paNoIr){
   
 }
 
